03019282bc4a1ce21d4ec16e0d31ef137ce7fa8d
Gryph0123 <gryphninja@gmail.com>
2024-11-15 10:54:52 -0500

    Uhhh... idk, I added movement and physics

---
+++ Assets/Controller.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float boostForce;
+    public float angularAcceleration;
+    public float maxAngularVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gets mouse position on screen
+        Vector3 mouseScreenPosition = Input.mousePosition;
+
+        Debug.DrawLine(transform.position, transform.position + transform.forward * 12, Color.red);
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity += boostForce / rb.mass * transform.forward * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.angularVelocity += -angularAcceleration * Time.deltaTime * transform.right;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.angularVelocity += angularAcceleration * Time.deltaTime * transform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.angularVelocity += -angularAcceleration * Time.deltaTime * transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.angularVelocity += angularAcceleration * 2 * Time.deltaTime * transform.forward;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.angularVelocity += angularAcceleration * 2 * Time.deltaTime * transform.up;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.angularVelocity += transform.up * -angularAcceleration * Time.deltaTime;
+        }
+
+    }
+}
---
+++ Assets/liftScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class liftScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float liftCoefficent;
+    public float handling;
+    public float attackIsZero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Calculates and applies lift
+        float attackAngle = Vector3.Angle(transform.forward, rb.velocity);
+        float attackCoefficent = ((float)Math.Tanh(-attackAngle * (attackAngle - attackIsZero) / 5) + 1f) / 2;
+        rb.velocity += attackCoefficent * Mathf.Pow(rb.velocity.magnitude, 2) * (liftCoefficent / rb.mass) * transform.up * Time.deltaTime;
+
+        // Shifts velocity towards heading
+        rb.velocity += (transform.forward * rb.velocity.magnitude - rb.velocity) * handling * Time.deltaTime;
+
+
+
+
+
+    }
+}
